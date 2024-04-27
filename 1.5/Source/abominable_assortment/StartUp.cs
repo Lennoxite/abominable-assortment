65eec8d94d0cdd314f5a08978c46db6479e9daf7
Lennoxicon <pinoy1999@gmail.com>
2024-04-27 04:48:18 -0700

    C# Solution Set Up

---
+++ 1.5/Source/abominable_assortment/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ABM_ASS
+{
+    [StaticConstructorOnStartup]
+    public static class ABOM_Assortment
+    {
+        static ABOM_Assortment()
+        {
+            Log.Message("Abominal Assortment - Ready!");
+        }
+    }
+}
